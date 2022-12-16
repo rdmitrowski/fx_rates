7743fde14722e8546c9300ad61d99bd406a76e5d
rdmitrowski <114663102+rdmitrowski@users.noreply.github.com>
2022-12-16 16:17:00 +0100

    Update process_fx_rates.py

--- fx_rates/process_fx_rates.py
+++ fx_rates/process_fx_rates.py
@@ -13,7 +13,9 @@ def process_files():
     df_merged = proc_def.dataframe_merge(df1, df2)
 
     output_file_name = proc_def.generate_filename()
-    df_merged.select(df_merged.effectiveDate, df_merged.no, explode(df_merged.rates)).select(col("effectiveDate"), col("no"), col("col.code"), col("col.currency"), col("col.mid")).coalesce(1).write.options(delimiter=';').mode('overwrite').csv(output_file_name)
+    # df_merged.select(df_merged.effectiveDate, df_merged.no, explode(df_merged.rates)).select(col("effectiveDate"), col("no"), col("col.code"), col("col.currency"), col("col.mid")).coalesce(1).write.options(delimiter=';').mode('overwrite').csv(output_file_name)
+    df_write = df_merged.select(df_merged.effectiveDate, df_merged.no, explode(df_merged.rates)).select(col("effectiveDate"), col("no"), col("col.code"), col("col.currency"), col("col.mid")).coalesce(1)
+    proc_def.write_output(df_write, output_file_name, ";", "overwrite")
 
 
 process_files()
