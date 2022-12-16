998205b159b200a491a06378dcfdf9830143702d
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 <rdmitrow@DELEQ0283300472.corp.capgemini.com>
2022-12-16 13:26:02 +0100

    Add configuration file

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+setuptools.setup(
+    name="fx_rates",
+    version="1.0.1",
+    author="Rafal Dmitrowski",
+    description="Generating fx_rates mix",
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=required,
+    python_requires='>=3.7'
+)
+
