533f8463b5acf677537ea813ef6cb01a726ba8d1
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 <rdmitrow@DELEQ0283300472.corp.capgemini.com>
2022-12-19 12:12:31 +0100

    Modify testing

--- tests/test_pytest.py
+++ tests/test_pytest.py
@@ -1,11 +1,13 @@
 # python -m pytest --import-mode=append tests/
+from pyspark.sql import DataFrame
+from chispa import assert_df_equality
+from pyspark.sql.types import StructType, StructField, FloatType, StringType, IntegerType
+
 import sys
 sys.path.insert(0, './fx_rates')
 
 import spark_sess
 import proc_def
-import process_fx_rates
-
 
 def test_spark_sess():
     sess = spark_sess.spark_sess
@@ -23,4 +25,33 @@ def test_generate_filename():
     assert file_name.startswith("fx_rates") and file_name.endswith(".csv")
 
 def test_dataframe_merge():
-    pass
+    spark = spark_sess.spark_sess()
+    df1 = spark.createDataFrame(
+        [("243/A/NBP/2022", "2022-12-16", "bat (Tajlandia)", "THB", 0.1265)],
+        StructType([StructField("no", StringType(), True),
+        StructField("effectiveDate", StringType(), True),
+        StructField("currency", StringType(), True),
+        StructField("code", StringType(), True),
+        StructField("mid", FloatType(), True)])
+        )
+    df2 = spark.createDataFrame(
+        [("243/A/NBP/2022", "2022-12-16", "dolar amerykański", "USD", 4.4227)],
+        #["no", "effectiveDate", "currency", "code", "mid"]
+        StructType([StructField("no", StringType(), True),
+        StructField("effectiveDate", StringType(), True),
+        StructField("currency", StringType(), True),
+        StructField("code", StringType(), True),
+        StructField("mid", FloatType(), True)])
+        )
+    df_expected =  spark.createDataFrame([
+        ("243/A/NBP/2022", "2022-12-16", "bat (Tajlandia)", "THB", 0.1265),
+        ("243/A/NBP/2022", "2022-12-16", "dolar amerykański", "USD", 4.4227)],     
+        #["no", "effectiveDate", "currency", "code", "mid"]
+        StructType([StructField("no", StringType(), True),
+        StructField("effectiveDate", StringType(), True),
+        StructField("currency", StringType(), True),
+        StructField("code", StringType(), True),
+        StructField("mid", FloatType(), True)])
+        )
+    df_result= proc_def.dataframe_merge(df1, df2)
+    assert_df_equality(df_result, df_expected)
